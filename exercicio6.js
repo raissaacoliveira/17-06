1972fd7e3a129855d6dbef90f79c19a51ebfc82d
RAISSA APARECIDA CORRÊA DE OLIVEIRA <raissa_ac_oliveira@estudante.sesisenai.org.br>
2026-06-17 16:30:35 -0300

    ex6

---
+++ exercicio6.js
@@ -0,0 +1,21 @@
+let opcao = 3;
+switch (opcao) {
+    case 1:
+        console.log("Setor: Financeiro");
+        break;
+
+        case 2:
+            console.log("Setor: Comercial");
+            break;
+
+            case 3:
+                console.log("Setor: Suporte Técnico");
+                break;
+
+                case 4:
+                    console.log("Setor: Recursos Humanos");
+                    break;
+
+                    default:
+                        console.log("Opção inválida.");
+}
