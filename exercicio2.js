9c956ef58e2adcb2aa92ab25f04a020df7204738
RAISSA APARECIDA CORRÊA DE OLIVEIRA <raissa_ac_oliveira@estudante.sesisenai.org.br>
2026-06-17 15:15:25 -0300

    ex2

---
+++ exercicio2.js
@@ -0,0 +1,7 @@
+let nome = "Raissa Oliveira";
+let cargo = "Auxiliar Administrativa";
+let experiencia = "1 ano";
+
+console.log("Nome: ", nome);
+console.log("Cargo: ", cargo);
+console.log("Experiencia: ", experiencia);
