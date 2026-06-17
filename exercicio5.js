1d02395ca8ba85de9b70e484e7d31c315edf2bcd
RAISSA APARECIDA CORRÊA DE OLIVEIRA <raissa_ac_oliveira@estudante.sesisenai.org.br>
2026-06-17 15:57:55 -0300

    ex5

---
+++ exercicio5.js
@@ -0,0 +1,10 @@
+let prioridade = 3;
+if (prioridade === 1) {
+    console.log("Prioridade: Baixo");
+} else if (prioridade === 2) {
+    console.log("Prioridade: Média");
+}else if (prioridade === 3) {
+    console.log("Prioridade: Alta");
+} else {
+    console.log("Código de prioridade inválido.")
+}
