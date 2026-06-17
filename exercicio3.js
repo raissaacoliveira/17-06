5b72af83de495c014bde509a2584fd729e69e113
RAISSA APARECIDA CORRÊA DE OLIVEIRA <raissa_ac_oliveira@estudante.sesisenai.org.br>
2026-06-17 15:21:57 -0300

    ex3

---
+++ exercicio3.js
@@ -0,0 +1,7 @@
+const vendas = 5000;
+const bonificacao = vendas * 0.10;
+const total = vendas + bonificacao;
+
+console.log("Vendas: R$ " + vendas);
+console.log("Bonificação: R$ " + bonificacao);
+console.log("Total: R$ " + total);
